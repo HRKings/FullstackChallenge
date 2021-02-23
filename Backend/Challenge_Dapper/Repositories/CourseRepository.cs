7ecfab423b8638bc112044692b3977b603a4c286
Helton Reis <heltonkreis@gmail.com>
2021-02-23 02:29:08 -0300

    [change]{!!!} Move all database connection strings to environment variables

--- Backend/Challenge_Dapper/Repositories/CourseRepository.cs
+++ Backend/Challenge_Dapper/Repositories/CourseRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Challenge_Dapper.Models;
 using Dapper;
@@ -11,7 +12,7 @@ namespace Challenge_Dapper.Repositories
 		public static async Task<IEnumerable<Course>> GetAll()
 		{
 			await using var connection =
-				new NpgsqlConnection("Host=localhost;Database=challenge;Username=postgres;Password=");
+				new NpgsqlConnection(Environment.GetEnvironmentVariable("DATABASE"));
 			await connection.OpenAsync();
 			var result = await connection.QueryAsync<Course>("SELECT * FROM course ORDER BY id");
 			await connection.CloseAsync();
@@ -21,7 +22,7 @@ namespace Challenge_Dapper.Repositories
 		public static async Task<int> Count()
 		{
 			await using var connection =
-				new NpgsqlConnection("Host=localhost;Database=challenge;Username=postgres;Password=");
+				new NpgsqlConnection(Environment.GetEnvironmentVariable("DATABASE"));
 			await connection.OpenAsync();
 			var result = await connection.QueryFirstAsync<int>("SELECT COUNT(*) FROM course");
 			await connection.CloseAsync();
@@ -31,7 +32,7 @@ namespace Challenge_Dapper.Repositories
 		public static async Task<IEnumerable<Course>> GetPaged(int page, int pageSize)
 		{
 			await using var connection =
-				new NpgsqlConnection("Host=localhost;Database=challenge;Username=postgres;Password=");
+				new NpgsqlConnection(Environment.GetEnvironmentVariable("DATABASE"));
 			int skip = (page - 1) * pageSize;
 			await connection.OpenAsync();
 			var result = await connection.QueryAsync<Course>("SELECT * FROM course ORDER BY id OFFSET @skip LIMIT @pageSize",
@@ -43,7 +44,7 @@ namespace Challenge_Dapper.Repositories
 		public static async Task<Course> Get(int id)
 		{
 			await using var connection =
-				new NpgsqlConnection("Host=localhost;Database=challenge;Username=postgres;Password=");
+				new NpgsqlConnection(Environment.GetEnvironmentVariable("DATABASE"));
 			await connection.OpenAsync();
 			var result =
 				await connection.QueryFirstAsync<Course>("SELECT * FROM course WHERE id = @ID", new {ID = id});
@@ -54,7 +55,7 @@ namespace Challenge_Dapper.Repositories
 		public static async Task<Course> Insert(string name)
 		{
 			await using var connection =
-				new NpgsqlConnection("Host=localhost;Database=challenge;Username=postgres;Password=");
+				new NpgsqlConnection(Environment.GetEnvironmentVariable("DATABASE"));
 
 			await connection.OpenAsync();
 			var result = await connection.QueryFirstAsync<Course>("INSERT INTO course(name) VALUES (@name)", new {name});
@@ -65,7 +66,7 @@ namespace Challenge_Dapper.Repositories
 		public static async Task<Course> Update(Course course)
 		{
 			await using var connection =
-				new NpgsqlConnection("Host=localhost;Database=challenge;Username=postgres;Password=");
+				new NpgsqlConnection(Environment.GetEnvironmentVariable("DATABASE"));
 			await connection.OpenAsync();
 			var result = await connection.QueryFirstAsync<Course>("UPDATE course SET name = @name WHERE id = @ID", new {name = course.Name, ID = course.Id});
 			await connection.CloseAsync();
@@ -75,7 +76,7 @@ namespace Challenge_Dapper.Repositories
 		public static async Task<Course> Delete(int id)
 		{
 			using var connection =
-				new NpgsqlConnection("Host=localhost;Database=challenge;Username=postgres;Password=");
+				new NpgsqlConnection(Environment.GetEnvironmentVariable("DATABASE"));
 
 			await connection.OpenAsync();
 			var result = await connection.QueryFirstAsync<Course>("DELETE FROM course WHERE id = @ID", new {ID = id});
